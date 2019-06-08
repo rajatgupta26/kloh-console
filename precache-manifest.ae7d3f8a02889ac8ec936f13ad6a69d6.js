b217a813795f35d99d7b1b875fd2a99dd93a7deb
rajatgupta26 <rajat.g@flipkart.com>
2019-06-08 18:15:42 +0530

    Updates

---
+++ precache-manifest.ae7d3f8a02889ac8ec936f13ad6a69d6.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "908be5c40ce33ec8b246",
+    "url": "http://kloh.in/static/css/main.80416753.chunk.css"
+  },
+  {
+    "revision": "908be5c40ce33ec8b246",
+    "url": "http://kloh.in/static/js/main.a098b00c.chunk.js"
+  },
+  {
+    "revision": "5b42410c5e69a670a3a9",
+    "url": "http://kloh.in/static/js/runtime~main.09696c51.js"
+  },
+  {
+    "revision": "f82cc42bc096abf29180",
+    "url": "http://kloh.in/static/js/2.d8aaf23e.chunk.js"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "http://kloh.in/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "http://kloh.in/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "http://kloh.in/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "http://kloh.in/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "http://kloh.in/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "fe82843c586d5f630f52e8d079b8bbe3",
+    "url": "http://kloh.in/static/media/logo_200.fe82843c.png"
+  },
+  {
+    "revision": "de88253696b81598494dd6bd5c0b087e",
+    "url": "http://kloh.in/static/media/100_6.de882536.jpg"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "http://kloh.in/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "b7f68e8afaec5bfad8fdcc44e4e4e483",
+    "url": "http://kloh.in/static/media/background_1920-11.b7f68e8a.jpg"
+  },
+  {
+    "revision": "acfe7046f47d96b9cb598622e50fe610",
+    "url": "http://kloh.in/static/media/background_1920-18.acfe7046.jpg"
+  },
+  {
+    "revision": "1420d15b22ed3f39abc468212d735814",
+    "url": "http://kloh.in/static/media/background_640-1.1420d15b.jpg"
+  },
+  {
+    "revision": "55e47e98ef3c84cf5eb4706a4c74311a",
+    "url": "http://kloh.in/static/media/background_640-3.55e47e98.jpg"
+  },
+  {
+    "revision": "1d98ff2e44d70e391ec7a49e4efdfa86",
+    "url": "http://kloh.in/static/media/background_1920-5.1d98ff2e.jpg"
+  },
+  {
+    "revision": "1e0eb5503400ca7f2a65175cec9e9115",
+    "url": "http://kloh.in/static/media/background_1920-6.1e0eb550.jpg"
+  },
+  {
+    "revision": "abf47fc19906e17185a37765e7594ee0",
+    "url": "http://kloh.in/static/media/background_1920-7.abf47fc1.png"
+  },
+  {
+    "revision": "22fc19f2c02f7dba20864f097f0bfd20",
+    "url": "http://kloh.in/static/media/background_1920-19.22fc19f2.jpg"
+  },
+  {
+    "revision": "a9a0ec6482226ea8098fc71a3fababe0",
+    "url": "http://kloh.in/static/media/product_640-1.a9a0ec64.jpg"
+  },
+  {
+    "revision": "da0b0457353c6555a5ba4535597f2187",
+    "url": "http://kloh.in/static/media/product_640-2.da0b0457.jpg"
+  },
+  {
+    "revision": "64ed27c9aca0306b4185b82e144cdedd",
+    "url": "http://kloh.in/static/media/product_640-3.64ed27c9.jpg"
+  },
+  {
+    "revision": "2110d8cd7d70f6792b4c2d517cb7f593",
+    "url": "http://kloh.in/static/media/product_640-4.2110d8cd.jpg"
+  },
+  {
+    "revision": "bf8840bd88a80c14418b238a7f1beb24",
+    "url": "http://kloh.in/static/media/product_640-5.bf8840bd.jpg"
+  },
+  {
+    "revision": "f9249755162af20121af258ca1ffc578",
+    "url": "http://kloh.in/static/media/product_640-6.f9249755.jpg"
+  },
+  {
+    "revision": "1dce100ffdb944226acf9db666a57319",
+    "url": "http://kloh.in/static/media/100_7.1dce100f.jpg"
+  },
+  {
+    "revision": "a4be0bbf3a08f9de24548aa6765af93f",
+    "url": "http://kloh.in/static/media/100_8.a4be0bbf.jpg"
+  },
+  {
+    "revision": "798c02eb53d02e11c3d8a6588cdf5033",
+    "url": "http://kloh.in/static/media/100_9.798c02eb.jpg"
+  },
+  {
+    "revision": "56d054ffdf15ac516d7ba19fa9a33ad5",
+    "url": "http://kloh.in/static/media/100_10.56d054ff.jpg"
+  },
+  {
+    "revision": "336faadc989ea15bbe2c0fb30e20bcd7",
+    "url": "http://kloh.in/static/media/100_11.336faadc.jpg"
+  },
+  {
+    "revision": "1e0889afa5d1fa653fef4f8b5d3272d2",
+    "url": "http://kloh.in/static/media/100_12.1e0889af.jpg"
+  },
+  {
+    "revision": "63265e929c9f7442ea1853d2dc19a188",
+    "url": "http://kloh.in/static/media/100_13.63265e92.jpg"
+  },
+  {
+    "revision": "595b8cbdd2b7f27cf7d38b895a9b56c9",
+    "url": "http://kloh.in/static/media/100_14.595b8cbd.jpg"
+  },
+  {
+    "revision": "f9ad7e195038e09286c0179895a86618",
+    "url": "http://kloh.in/index.html"
+  }
+];
